95196829ad6672a339810c52b0ea08e2f5a38ec0
evanburchard <evan.burchard@gmail.com>
2015-09-27 00:11:23 -0700

    updated pins and keys for layout

--- modifooter.ino
+++ modifooter.ino
@@ -2,28 +2,28 @@ const int buttonOnePin = 1;
 const int buttonTwoPin = 2;
 const int buttonThreePin = 3;
 const int buttonFourPin = 4;
-                
+
 void setup() {
   pinMode(buttonOnePin, INPUT);
   pinMode(buttonTwoPin, INPUT);
   pinMode(buttonThreePin, INPUT);
   pinMode(buttonFourPin, INPUT);
-  pinMode(13, OUTPUT);
+  pinMode(0, OUTPUT);
   delay(100);
-  digitalWrite(13, 1);
-  delay(100);                   
-  digitalWrite(13, 0);
+  digitalWrite(0, 1);
   delay(100);
-  digitalWrite(13, 1);
+  digitalWrite(0, 0);
   delay(100);
-  digitalWrite(13, 0);
+  digitalWrite(0, 1);
   delay(100);
-  digitalWrite(13, 1);
+  digitalWrite(0, 0);
   delay(100);
-  digitalWrite(13, 0);
+  digitalWrite(0, 1);
   delay(100);
-  digitalWrite(13, 1);
-}      
+  digitalWrite(0, 0);
+  delay(100);
+  digitalWrite(0, 1);
+}
 
 void loop() {
   delay(10);
@@ -32,11 +32,13 @@ void loop() {
   bool buttonThreePressed = digitalRead(buttonThreePin);
   bool buttonFourPressed = digitalRead(buttonFourPin);
 
+//    (buttonFourPressed ? MODIFIERKEY_SHIFT : 0) |
+//    (buttonTwoPressed ? MODIFIERKEY_ALT : 0)
   Keyboard.set_modifier(
-    (buttonOnePressed ? MODIFIERKEY_GUI : 0) |
-    (buttonTwoPressed ? MODIFIERKEY_SHIFT : 0) |
-    (buttonThreePressed ? MODIFIERKEY_CTRL : 0) |
-    (buttonFourPressed ? MODIFIERKEY_ALT : 0)
+    (buttonOnePressed ? MODIFIERKEY_CTRL : 0) |
+    (buttonThreePressed ? MODIFIERKEY_SHIFT : 0) |
+    (buttonFourPressed ? MODIFIERKEY_ALT : 0) |
+    (buttonTwoPressed ? MODIFIERKEY_GUI : 0)
   );
   Keyboard.send_now();
 }
